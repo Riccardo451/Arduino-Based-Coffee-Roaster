d2be308199ca2e34162e01e664a2b1e823cdd144
Riccardo Montaguti <ricky451@hotmail.it>
2026-07-22 12:01:26 +0200

    Update Arduino-Based-Coffee-Roaster_Experimental_EMA.ino

--- Arduino-Based-Coffee-Roaster_Experimental/Arduino-Based-Coffee-Roaster_Experimental_EMA.ino
+++ Arduino-Based-Coffee-Roaster_Experimental/Arduino-Based-Coffee-Roaster_Experimental_EMA.ino
@@ -210,7 +210,7 @@ void tempReading()
     }
     else
     {
-        emaTemp += (temperature - emaTemp) >> 3; // >> 1 Fast Filter and rapid changes, >> 5 Slow filter high Smoothness
+        emaTemp += (temperature - emaTemp) >> 3; // smoothing factor of 1/2^N ->  >> 1 Fast, >> 5 Slow filter
     }
 
     uint16_t filteredTemp = (uint16_t)emaTemp;
